63976f91a49d4a127a6420cb1f852d917e43fbf0
SreenivasKasulanati <147663183+SreenivasKasulanati@users.noreply.github.com>
2023-12-01 20:34:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function predict() {
+    // Get form data and perform prediction logic
+    // Update the result in the #predictionResult element
+    var predictionResult = "Prediction: Healthy"; // Replace with your actual prediction logic
+    document.getElementById("predictionResult").innerText = predictionResult;
+}
